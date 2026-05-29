2185b9d8dbe5f408d95c12d3ef063f6357219b15
Deep Dwivedi <deepdwivedi64@gmail.com>
2026-05-29 13:18:21 +0530

    Added Day 7 Leap Year Checker Project

---
+++ Leap-Year-Checker/script.js
@@ -0,0 +1,8 @@
+let year = Number(prompt("Enter a year : "))
+
+if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+    console.log(year + " is a Leap Year")
+}
+else{
+    console.log(year + " is not a Leap Year")
+}
